1bbb82f6baea95dcecad0e805a3e28f5087ffedd
bz888 <ziyu.ben158@gmail.com>
2023-09-11 14:25:23 +1200

    re-export parseCotnractPath

--- packages/common-ethereum/src/codegen/utils.ts
+++ packages/common-ethereum/src/codegen/utils.ts
@@ -2,7 +2,11 @@
 // SPDX-License-Identifier: GPL-3.0
 
 import {loadFromJsonOrYaml} from '@subql/common';
+import {parseContractPath} from 'typechain';
 import {abiInterface} from './codegen-controller';
+
+// Re-export for generate command
+export {parseContractPath};
 
 export function loadReadAbi(filePath: string): abiInterface[] | {abi: abiInterface[]} {
   return loadFromJsonOrYaml(filePath) as abiInterface[] | {abi: abiInterface[]};
